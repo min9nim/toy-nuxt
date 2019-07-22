c86dcc1981d00fdbbfebd6343ed9c0f262067c7f
mgsong <mgsong@jjsoft.kr>
2019-07-22 09:57:48 +0900

    use v-for

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,10 +1,7 @@
-<template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two" />
-    <div class="Triangle Triangle--one" />
-    <div class="Triangle Triangle--three" />
-    <div class="Triangle Triangle--four" />
-  </div>
+<template lang="pug">
+  .VueToNuxtLogo
+    template(v-for="val in arr")
+      .Triangle(:class="'Triangle--'+val")
 </template>
 
 <script lang="ts">
@@ -12,60 +9,51 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class extends Vue {}
+export default class extends Vue {
+  arr = ['one', 'two', 'three', 'four']
+}
 </script>
 
-<style>
-.VueToNuxtLogo {
-  display: inline-block;
-  animation: turn 2s linear forwards 1s;
-  transform: rotateX(180deg);
-  position: relative;
-  overflow: hidden;
-  height: 180px;
-  width: 245px;
-}
-
-.Triangle {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-}
-
-.Triangle--one {
-  border-left: 105px solid transparent;
-  border-right: 105px solid transparent;
-  border-bottom: 180px solid #41b883;
-}
-
-.Triangle--two {
-  top: 30px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 87.5px solid transparent;
-  border-right: 87.5px solid transparent;
-  border-bottom: 150px solid #3b8070;
-}
-
-.Triangle--three {
-  top: 60px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 70px solid transparent;
-  border-right: 70px solid transparent;
-  border-bottom: 120px solid #35495e;
-}
-
-.Triangle--four {
-  top: 120px;
-  left: 70px;
-  animation: godown 0.5s linear forwards 3s;
-  border-left: 35px solid transparent;
-  border-right: 35px solid transparent;
-  border-bottom: 60px solid #fff;
-}
+<style lang="stylus">
+.VueToNuxtLogo
+  display inline-block
+  animation turn 2s linear forwards 1s
+  transform rotateX(180deg)
+  position relative
+  overflow hidden
+  height 180px
+  width 245px
+.Triangle
+  position absolute
+  top 0
+  left 0
+  width 0
+  height 0
+.Triangle--one
+  border-left 105px solid transparent
+  border-right 105px solid transparent
+  border-bottom 180px solid #41b883
+.Triangle--two
+  top 30px
+  left 35px
+  animation goright 0.5s linear forwards 3.5s
+  border-left 87.5px solid transparent
+  border-right 87.5px solid transparent
+  border-bottom 150px solid #3b8070
+.Triangle--three
+  top 60px
+  left 35px
+  animation goright 0.5s linear forwards 3.5s
+  border-left 70px solid transparent
+  border-right 70px solid transparent
+  border-bottom 120px solid #35495e
+.Triangle--four
+  top 120px
+  left 70px
+  animation godown 0.5s linear forwards 3s
+  border-left 35px solid transparent
+  border-right 35px solid transparent
+  border-bottom 60px solid #fff
 
 @keyframes turn {
   100% {
